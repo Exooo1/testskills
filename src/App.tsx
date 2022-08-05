3f0f487ed633c2283500a18b0823291fd7983eee
Maskalenchik Vlas <vlasmaskalenchik1998@gmail.com>
2022-08-05 16:19:16 +0300

    test react

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.css'
 import {NumberT} from "./Components/Flangan/Number/Number";
 import {String} from "./Components/Flangan/String/String";
 import {SvgTest} from "./Components/SVG/SvgTest";
+
+const Buber = ({element}: any) => {
+    return <div>
+        <h1>Buber</h1>
+        {element}
+    </div>
+}
+
 export const App = () => {
+    let [age, setage] = useState(0)
+
+    age = 24
     return <div>
         <SvgTest/>
+        <h1>My name is {age} </h1>
+        <Buber element={<input/>}/>
         {/*<Functions/>*/}
         {/*<FuntioncGenerators/>*/}
         {/*<String/>*/}
