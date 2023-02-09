0f561cb6add5c61716510ba4a8107df76be519a0
Maskalenchik Vlas <vlasmaskalenchik1998@gmail.com>
2023-02-09 22:31:38 +0300

    asynch setState

--- src/Components/Timer/timer.tsx
+++ src/Components/Timer/timer.tsx
@@ -4,15 +4,22 @@ export const Timer = () => {
     const [time, setTime] = useState(0)
     const [isActive, setIsActive] = useState<boolean>(false)
     useEffect(() => {
-        console.log('Hello')
         let timeId: any;
         if (isActive) timeId = setInterval(() => setTime(state => state + 1), 1000)
         return () => clearInterval(timeId)
     }, [isActive])
     const handlerIsActive = (value: boolean) => setIsActive(state => state = value)
+    const handlerTime = () => {
+        setTime(state => state + 1)
+        setTime(state => state + 1)
+        setTime(state => state + 1)
+        setTime(state => state + 1)
+
+    }
     return <section>
         <h1>Time: {time}</h1>
         <button onClick={() => handlerIsActive(true)}>start</button>
         <button onClick={() => handlerIsActive(false)}>stop</button>
+        <button onClick={handlerTime}>click</button>
     </section>
 }
