0c7d7290cfda2e9f738f74bf09af8f6d53adcda8
Maskalenchik Vlas <vlasmaskalenchik1998@gmail.com>
2023-02-16 17:56:28 +0300

    added

--- src/Components/Card/card.tsx
+++ src/Components/Card/card.tsx
@@ -1,52 +1,49 @@
 import styles from './card.module.scss'
-import {useState} from "react";
+import {ChangeEvent, useRef, useState} from "react";
 
 export const Card = () => {
     const [one, setOne] = useState<string>('')
     const [two, setTwo] = useState<string>('')
     const [three, setThree] = useState<string>('')
-    const [four, setFour] = useState<string>('')
-    const [name, setName] = useState<string>('')
-    const handlerReset = () => {
-        setOne('')
-        setTwo('')
-        setThree('')
-        setFour('')
-    }
-    const handlerNumber = (e: any) => {
-        setName(name + e.target.value)
-        console.log(e.target.value.length + name.length)
-        switch (e.target.value.length + name.length) {
+    const [focus, setFocus] = useState<string>('')
+    const handlerFocus = (e: any) => setFocus(e.target.value)
+    const refOne = useRef(null);
+    const refTwo = useRef(null);
+    const refThree = useRef(null);
+    const switcher = (e: ChangeEvent<HTMLInputElement>) => {
+        let find = e.target.value.length + one.length + two.length
+        if (e.target.value.length === 0) {
+            find = one.length + two.length + three.length
+        }
+        switch (find) {
             case 1:
-                setOne(e.target.value[0])
+                setOne(e.target.value)
+                if (!e.target.value) break
+                else (e.target.nextElementSibling as HTMLElement).focus()
                 break;
             case 2:
-                setTwo(e.target.value[0])
+                setTwo(e.target.value)
+                if (!e.target.value) {
+                    (e.target.previousElementSibling as HTMLElement).focus()
+                } else (e.target.nextElementSibling as HTMLElement).focus()
                 break;
             case 3:
-                console.log()
-                setThree(e.target.value[0])
-                break;
-            case 4:
-                setFour(e.target.value[0])
+                setThree(e.target.value)
+                if (!e.target.value) (e.target.previousElementSibling as HTMLElement).focus()
                 break;
         }
     }
     return <section className={styles.card}>
         <section className={styles.card_content}>
             <h1>Card</h1>
-            <section>
-                <section>
-                    {one ? <p className={styles.anim}>{one}</p> :
-                        <input maxLength={4} onChange={handlerNumber} type="text"/>}
-                    {two ? <p className={styles.anim}>{two}</p> :
-                        <input maxLength={4} onChange={handlerNumber} type="text"/>}
-                    {three ? <p className={styles.anim}>{three}</p> :
-                        <input maxLength={4} onChange={handlerNumber} type="text"/>}
-                    {four ? <p className={styles.anim}>{four}</p> :
-                        <input maxLength={4} onChange={handlerNumber} type="text"/>}
-                </section>
-                <button onClick={handlerReset}>Reset</button>
+            <div>
+                <input type="text" maxLength={1} value={one} ref={refOne} autoFocus={true} onChange={switcher}/>
+                <input type="text" maxLength={1} value={two} ref={refTwo} onChange={switcher}/>
+                <input type="text" maxLength={1} value={three} ref={refThree} onChange={switcher}/>
+            </div>
+            <section className={styles.border}>
+                <div className={focus.length >= 1 ? styles.borderActive : ''}><h1>Here: {focus}</h1></div>
+                <input type="text" value={focus} onChange={handlerFocus}/>
             </section>
         </section>
     </section>
