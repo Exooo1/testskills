eaad1ae3bd9c50919b4beb6fdfbb7b5af2a3f462
Maskalenchik Vlas <vlasmaskalenchik1998@gmail.com>
2023-02-20 12:13:41 +0300

    new anim

--- src/Components/Card/card.tsx
+++ src/Components/Card/card.tsx
@@ -6,15 +6,13 @@ export const Card = () => {
     const [two, setTwo] = useState<string>('')
     const [three, setThree] = useState<string>('')
     const [focus, setFocus] = useState<string>('')
+    const [isActive, setIsActive] = useState<boolean>(false)
     const handlerFocus = (e: any) => setFocus(e.target.value)
     const refOne = useRef(null);
     const refTwo = useRef(null);
     const refThree = useRef(null);
     const switcher = (e: ChangeEvent<HTMLInputElement>) => {
-        let find = e.target.value.length + one.length + two.length
-        if (e.target.value.length === 0) {
-            find = one.length + two.length + three.length
-        }
+        let find = e.target.value.length + one.length + two.length + three.length
         switch (find) {
             case 1:
                 setOne(e.target.value)
@@ -46,6 +44,23 @@ export const Card = () => {
                 <input type="text" value={focus} onChange={handlerFocus}/>
                 <h1 className={focus && styles.h1}>Hello my friend</h1>
             </section>
+        </section>
+        <section className={styles.block} onClick={() => setIsActive(!isActive)}>
+            <div className={isActive ? styles.isActive : ''}>
+                <h1>Hello</h1>
+                <p>2</p>
+                <p>2</p>
+                <p>2</p>
+                <p>2</p>
+            </div>
+        </section>
+        <section className={styles.block2}>
+            <p>1</p>
+            <ul className={isActive?styles.active:''}>
+                <li>1</li>
+                <li>2</li>
+                <li>34</li>
+            </ul>
         </section>
     </section>
 }
